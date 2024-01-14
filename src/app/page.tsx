66ced7f510d26161987a8f9b979ed6002c0fbb40
eug-vs <eugene@eug-vs.xyz>
2024-01-14 05:51:51 +0100

    feat: parse taskwarrior JSON data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,138 @@
-import Image from 'next/image'
+import util from "util";
+import { exec as execRaw } from "child_process";
+import { z } from "zod";
+import Link from "next/link";
+import _ from "lodash";
+const exec = util.promisify(execRaw);
 
-export default function Home() {
+interface Props {
+  searchParams: {
+    cmd?: string;
+  };
+}
+
+const timestampSchema = z.string().transform((timestamp) => {
+  // Convert taskwarrior timestamp to JS date
+  const isoFormat = timestamp.replace(
+    /(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})Z/,
+    "$1-$2-$3T$4:$5:$6Z",
+  );
+  return new Date(isoFormat);
+});
+
+const annotationSchema = z.object({
+  entry: timestampSchema,
+  description: z.string(),
+});
+export type Annotation = z.infer<typeof annotationSchema>;
+
+const taskSchema = z.object({
+  id: z.number(),
+  uuid: z.string(),
+  entry: timestampSchema,
+  modified: timestampSchema,
+  due: timestampSchema.optional(),
+  end: timestampSchema.optional(),
+  description: z.string(),
+  status: z.string(),
+  project: z.string().optional(),
+  tags: z.string().array().optional(),
+  annotations: annotationSchema.array().optional(),
+  urgency: z.number(),
+});
+
+export type Task = z.infer<typeof taskSchema>;
+
+function MaybeLink({ text: textOrUrl }: { text: string }) {
+  try {
+    new URL(textOrUrl); // Will throw for invalid URLs
+    return (
+      <Link className="text-blue-500 underline" href={textOrUrl}>
+        {textOrUrl}
+      </Link>
+    );
+  } catch {
+    return <>{textOrUrl}</>;
+  }
+}
+
+function Annotation({ annotation }: { annotation: Annotation }) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <li>
+      {annotation.entry.toLocaleString()}{" "}
+      <MaybeLink text={annotation.description} />
+    </li>
+  );
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+function TaskRow({ task }: { task: Task }) {
+  return (
+    <li className="p-2 shadow-sm border">
+      <span className="font-bold">{task.urgency.toFixed(2)}</span>{" "}
+      {task.description}{" "}
+      {task.project && (
+        <span className="bg-green-300 rounded-md py-0.5 px-1 text-sm">
+          {task.project}
+        </span>
+      )}
+      {task.tags && (
+        <>
+          {task.tags.map((tag) => (
+            <span
+              key={tag}
+              className="bg-blue-300 rounded-md py-0.5 px-1 text-sm"
+            >
+              {tag}
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          ))}
+        </>
+      )}
+      {task.status}
+      {task.due && (
+        <span className="bg-red-300 rounded-md py-0.5 px-1 text-sm">
+          {task.due?.toLocaleString()}
+        </span>
+      )}
+      {task.annotations && (
+        <ul className="text-gray-500">
+          {task.annotations.map((anno) => (
+            <Annotation key={anno.description} annotation={anno} />
+          ))}
+        </ul>
+      )}
+    </li>
+  );
+}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+async function CmdOutput({ cmd }: { cmd: string }) {
+  const { stdout, stderr } = await exec(cmd);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+  try {
+    const json = JSON.parse(stdout);
+    console.log({ json });
+    const parsed = taskSchema.array().parse(json);
+    return (
+      <ul className="flex gap-4 flex-col">
+        {_.orderBy(parsed, "urgency", "desc").map((task) => (
+          <TaskRow key={task.uuid} task={task} />
+        ))}
+      </ul>
+    );
+  } catch (e) {
+    return (
+      <section className="border max-w-[70vw] mx-auto overflow-x-scroll">
+        <pre>{stdout}</pre>
+        <pre className="bg-red-500 text-white">{stderr}</pre>
+      </section>
+    );
+  }
+}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+export default async function Home({ searchParams }: Props) {
+  return (
+    <main className="flex flex-col items-center justify-between p-24">
+      <h1 className="font-mono">{searchParams.cmd}</h1>
+      {searchParams.cmd && <CmdOutput cmd={searchParams.cmd} />}
     </main>
-  )
+  );
 }
